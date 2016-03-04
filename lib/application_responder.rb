08e490cc7fcf4e7b0a5db7ca07e4ba38cc64b84f
shane989 <shane989@tamu.edu>
2016-03-04 07:39:51 +0000

    Changed responder file

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -1,7 +1,6 @@
 class ApplicationResponder < ActionController::Responder
   include Responders::FlashResponder
   include Responders::HttpCacheResponder
-  include ActionController::RespondWith
 
   # Redirects resources to the collection path (index action) instead
   # of the resource path (show action) for POST/PUT/DELETE requests.
